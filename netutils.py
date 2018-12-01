c47e47026544b985b0a5edecbe7f25e8b5b2ebda
Kazuya Takei <attakei@gmail.com>
2018-12-02 03:19:38 +0900

    Ready for multiple record type

--- netutils.py
+++ netutils.py
@@ -2,13 +2,29 @@
 import dns.resolver
 from errbot import BotPlugin, arg_botcmd
 
+DNS_RECORD_TYPES = [
+    'A',
+    # TODO: Implement after
+    # 'AAAA',
+    # 'CNAME',
+    # 'MX',
+    # 'SOA',
+    # 'TXT',
+]
+
+DNS_RECORD_TYPE_CHOICES = DNS_RECORD_TYPES \
+    + [t.lower() for t in DNS_RECORD_TYPES]
+
 
 class Netutils(BotPlugin):
     """Net util commands."""
 
     @arg_botcmd('fqdn', type=str)
-    def dig(self, message, fqdn):
+    @arg_botcmd(
+        '-t', dest='record_type', type=str,
+        choices=DNS_RECORD_TYPE_CHOICES, default='A')
+    def dig(self, message, fqdn, record_type):
         """Look up DNS record."""
-        answers = dns.resolver.query(fqdn, 'A')
+        answers = dns.resolver.query(fqdn, record_type)
         return f'**Answers for {fqdn}**\n\n' \
             + '\n'.join([f'* {r.address}' for r in answers])
