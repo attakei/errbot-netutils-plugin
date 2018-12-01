639f5d93342550cc1b41d0e9244bb01d8658550e
Kazuya Takei <attakei@gmail.com>
2018-12-02 02:07:48 +0900

    Fix for pycodestyle

--- netutils.py
+++ netutils.py
@@ -1,16 +1,14 @@
-"""Errbot netutils plugin
-"""
+"""Errbot netutils plugin."""
 import dns.resolver
 from errbot import BotPlugin, arg_botcmd
 
 
 class Netutil(BotPlugin):
-    """Net util commands
-    """
+    """Net util commands."""
+
     @arg_botcmd('fqdn', type=str)
     def dig(self, message, fqdn):
-        """Look up DNS record
-        """
+        """Look up DNS record."""
         answers = dns.resolver.query(fqdn, 'A')
         return f'**Answes for {fqdn}**\n\n' \
             + '\n'.join([f'* {r.address}' for r in answers])
